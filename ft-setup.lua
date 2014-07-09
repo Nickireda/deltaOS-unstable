43b9941d107ef368644fd0cb2b8e04e4b8bf7c24
FlareHAX0R <gameexpertnetwork@gmail.com>
2014-07-08 20:36:10 -0400

    Update ft-setup.lua

--- ft-setup.lua
+++ ft-setup.lua
@@ -1,5 +1,45 @@
 
 local function setup()
+  local function pocketDetect()
+  if pocket then
+    graphics.reset(colors.lighBlue, colors.white)
+    print("")
+    graphics.cPrint("WARNING")
+    print("")
+    print("We have detect you are running on a PDA.")
+    print("DeltaOS may be buggy on PDA's")
+    print("This may be fixed in the future.")
+    
+    paintutils.drawLine(2, kernel.y/2, kernel.x-1, kernel.y/2, colors.blue)
+    
+    paintutils.drawLine(2, kernel.y/2+3, kernel.x-1, kernel.y/2+3, colors.blue)
+    
+    term.setCursorPos(2, kernel.y/2+3)
+    print("Uninstall DeltaOS")
+    
+    term.setCursorPos(2, kernel.y/2)
+    print("Continue anyways...")
+    
+    while true do
+      local e, b, x, y = os.pullEvent("mouse_click")
+      if y == kernel.y/2 and x >= 2 and x <= kernel.x-1 then
+        return
+      elseif y == kernel.y/2+3 and x >= 2 and x <= kernel.x-1 then
+        fs.delete("/apps")
+        fs.delete("/system")
+        fs.delete("/users")
+        fs.delete("/startup")
+        fs.delete("/apis")
+        os.reboot()
+      end
+  end
+end
+
+pocketDetect()
+    
+  
+  
+  
 graphics.reset(colors.lightGray, colors.black)
 graphics.cPrint("DeltaOS first-time setup")
 
@@ -70,7 +110,7 @@ os.reboot()
 end
 
 local x = kernel.catnip(setup)
-if x ~= "noErr" then 
+if x ~= "noErr" or x ~= "nil" then 
   graphics.reset(colors.blue, colors.white)
   print("")
   term.setBackgroundColor(colors.white)
